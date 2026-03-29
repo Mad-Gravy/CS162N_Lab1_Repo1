807c59d51b367ffa6b628ea57d19284bbe27b58d
Joseph Paul Teague <graveytea@gmail.com>
2026-03-29 12:41:53 -0700

    Starter Files, Initial Upload

---
+++ StringsLab_Core5/ShiftCypher/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShiftCypher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Changed the output to Hello Class
+            Console.WriteLine("Hello Class!");
+        }
+    }
+}
