f04083899591a59b012210ea2402753412c11791
Joseph Teague <graveytea@gmail.com, teaguej@my.lanecc.edu>
2026-04-07 13:50:12 -0700

    Thought I had Ex 5 and 6 figured out, but now I realize my cypher can turn letters into punctuation so gonna have to figure that out...

--- StringsLab_Core5/ShiftCypher/Program.cs
+++ StringsLab_Core5/ShiftCypher/Program.cs
@@ -6,10 +6,59 @@ namespace ShiftCypher
 {
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main()
         {
-            //Changed the output to Hello Class
-            Console.WriteLine("Hello Class!");
+            //Set a shift value
+            Random rng = new Random();
+            int shift = rng.Next(-10, 11);
+
+            // Deliver instructions and acquire input
+            Console.WriteLine("Let's lay with Shift Cyphers! Or 'Caesar Cyphers,' if you prefer!");
+            Console.WriteLine("Give us a word you would like to encrypt with the Cypher: ");
+            string input = Console.ReadLine();
+
+            // Pass input and shift to the encryption method
+            string output = WordEncryption(input, shift);
+
+            // Deliver result
+            Console.WriteLine($"After encrypting your input with a cypher shift of {shift}, your input becomes: ");
+            Console.WriteLine(output);
+
+        }
+
+        //---- Methods ----------------------------------------
+
+        static string WordEncryption(string input, int shift)
+        {
+            string output = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                char letter = input[i];
+                int ascii = (int)letter;
+
+                if (letter == ' ')
+                {
+                    output += letter;
+                }
+                else if (char.IsPunctuation(letter))
+                {
+                    output += letter;
+                }
+                else if (letter == 'z')
+                {
+                    output += (char)('a' + (shift - 1));
+                }
+                else if (letter == 'Z')
+                {
+                    output += (char)('A' + (shift - 1));
+                }
+                else
+                {
+                    output += (char)(letter + shift);
+                }
+            }
+            return output;
         }
     }
 }
